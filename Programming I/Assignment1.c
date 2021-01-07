7cfbd51dce92f10bbc5470f4930c6dd1f951b67a
s-touchstone <43253461+s-touchstone@users.noreply.github.com>
2021-01-07 14:42:12 -0600

    Add files via upload

---
+++ Programming I/Assignment1.c
@@ -0,0 +1,16 @@
+//Steven Touchstone
+/*Programming 1
+August 25, 2016*/
+//Professor Vladimir Ufimtsev
+//Prints my Information
+
+#include<stdio.h>
+	
+main()
+{
+	printf("My name is Steven Touchstone\n");
+	printf("My birthday is on February the 16th\n");
+	printf("I am from Elk City, Oklahoma\n");
+	
+	return 0;
+}
